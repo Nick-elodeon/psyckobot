36a9d2b3e6e36f52346b2e536e003e07b8942c36
Jeļena Vetčinņikova <nikolai.vetchinnikov@gmail.com>
2022-10-11 01:14:17 +0300

    Improvements

--- responses.py
+++ responses.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-def sample_responses(message):
-    user_message = str(message).lower()
+def sample_responses(input_text):
+    user_message = str(input_text).lower()
 
     if user_message in ("Привет", "Привет!", "Прив"):
         return "Привет!"
